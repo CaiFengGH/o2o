3fc534bf81c4603f0437e37bf2428b3c0fd12472
Zhiwei Lv <lzwheal@gmail.com>
2017-09-26 19:16:52 +0800

    modify format

--- application/bis/controller/Location.php
+++ application/bis/controller/Location.php
@@ -23,48 +23,48 @@ class Location extends Base
  	*/
 	public function add(){
 		if(request()->isPost()){
-			// 第一点 检验数据 tp5 validate机制， 小伙伴自行完成
+		// 第一点 检验数据 tp5 validate机制， 小伙伴自行完成
 
-            $data = input('post.');
-            $bisId = $this->getLoginUser()->bis_id;
-            
-            $data['cat'] = '';
-            if(!empty($data['se_category_id'])) {
-                $data['cat'] = implode('|', $data['se_category_id']);
-            }
+		    $data = input('post.');
+		    $bisId = $this->getLoginUser()->bis_id;
 
-            // 获取经纬度
-            $lnglat = \Map::getLngLat($data['address']);
-            if(empty($lnglat) || $lnglat['status'] !=0) {
-                $this->error('无法获取数据，或者匹配的地址不精确');
-            }
+		    $data['cat'] = '';
+		    if(!empty($data['se_category_id'])) {
+			$data['cat'] = implode('|', $data['se_category_id']);
+		    }
 
-            // 门店入库操作
-            // 总店相关信息入库
-            $locationData = [
-                'bis_id' => $bisId,
-                'name' => $data['name'],
-                'logo' => $data['logo'],
-                'tel' => $data['tel'],
-                'contact' => $data['contact'],
-                'category_id' => $data['category_id'],
-                'category_path' => $data['category_id'] . ',' . $data['cat'],
-                'city_id' => $data['city_id'],
-                'city_path' => empty($data['se_city_id']) ? $data['city_id'] : $data['city_id'].','.$data['se_city_id'],
-                'api_address' => $data['address'],
-                'open_time' => $data['open_time'],
-                'content' => empty($data['content']) ? '' : $data['content'],
-                'is_main' => 0,
-                'status' => 1,
-                'xpoint' => empty($lnglat['result']['location']['lng']) ? '' : $lnglat['result']['location']['lng'],
-                'ypoint' => empty($lnglat['result']['location']['lat']) ? '' : $lnglat['result']['location']['lat'],
-            ];
-            $locationId = model('BisLocation')->add($locationData);
-            if($locationId) {
-                return $this->success('门店申请成功',url('location/add'));
-            }else {
-                return $this->error('门店申请失败',url('location/add'));
-            }
+		    // 获取经纬度
+		    $lnglat = \Map::getLngLat($data['address']);
+		    if(empty($lnglat) || $lnglat['status'] !=0) {
+			$this->error('无法获取数据，或者匹配的地址不精确');
+		    }
+
+		    // 门店入库操作
+		    // 总店相关信息入库
+		    $locationData = [
+			'bis_id' => $bisId,
+			'name' => $data['name'],
+			'logo' => $data['logo'],
+			'tel' => $data['tel'],
+			'contact' => $data['contact'],
+			'category_id' => $data['category_id'],
+			'category_path' => $data['category_id'] . ',' . $data['cat'],
+			'city_id' => $data['city_id'],
+			'city_path' => empty($data['se_city_id']) ? $data['city_id'] : $data['city_id'].','.$data['se_city_id'],
+			'api_address' => $data['address'],
+			'open_time' => $data['open_time'],
+			'content' => empty($data['content']) ? '' : $data['content'],
+			'is_main' => 0,
+			'status' => 1,
+			'xpoint' => empty($lnglat['result']['location']['lng']) ? '' : $lnglat['result']['location']['lng'],
+			'ypoint' => empty($lnglat['result']['location']['lat']) ? '' : $lnglat['result']['location']['lat'],
+		    ];
+		    $locationId = model('BisLocation')->add($locationData);
+		    if($locationId) {
+			return $this->success('门店申请成功',url('location/add'));
+		    }else {
+			return $this->error('门店申请失败',url('location/add'));
+            	    }
 		}else{
 			//获取城市的数据
 			$citys = model('City')->getNormalCityByParentId();
