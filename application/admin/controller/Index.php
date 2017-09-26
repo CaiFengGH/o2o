d21e0cb144500d0f3371772a994ed228a051085c
Zhiwei Lv <lzwheal@gmail.com>
2017-09-26 19:13:26 +0800

    Update Index.php

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -7,7 +7,7 @@ class Index extends Controller
 	//后台主页面
     public function index()
     {
-		return $this->fetch();
+	return $this->fetch();
     }
 	//后台主页面的显示
     public function welcome()
@@ -15,11 +15,11 @@ class Index extends Controller
     	//邮箱验证失败
 //    	\phpmailer\Email::send(1,1,1);
 //    	return "发送邮件成功";
-		return "Welcome o2o.caifeng.com";
+	return "Welcome o2o.caifeng.com";
     }
     //百度地图经纬度获取测试
     public function test()
     {
-  		return  \Map::staticimage("河南济源");
+  	return  \Map::staticimage("河南济源");
     }
 }
