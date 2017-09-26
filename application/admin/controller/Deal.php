15cb6166d9b5ca1174c2796b0209dd89479b91d9
Zhiwei Lv <lzwheal@gmail.com>
2017-09-26 19:12:58 +0800

    modify format

--- application/admin/controller/Deal.php
+++ application/admin/controller/Deal.php
@@ -19,32 +19,32 @@ class Deal extends Controller
 	 	$sdata = [];
 	 	//对日期进行验证
 	 	if(!empty($data['start_time']) && !empty($data['end_time']) && strtotime($data['end_time']) > strtotime($data['start_time'])) {
-	    	$sdata['create_time'] = [
-	    		['gt', strtotime($data['start_time'])],
-	    		['lt', strtotime($data['end_time'])],
-	    	];
-	    }
+	    		$sdata['create_time'] = [
+	    			['gt', strtotime($data['start_time'])],
+	    			['lt', strtotime($data['end_time'])],
+	    		];
+	    	}
 	 	//分别对分类 城市 商户名进行验证
 	 	if(!empty($data['category_id'])) {
 	    	$sdata['category_id'] = $data['category_id'];
-	    }
-	    if(!empty($data['city_id'])) {
-	    	$sdata['city_id'] = $data['city_id'];
-	    }
-	    if(!empty($data['name'])) {
-	    	$sdata['name'] = ['like', '%'.$data['name'].'%'];
-	    }
+	    	}
+	    	if(!empty($data['city_id'])) {
+	    		$sdata['city_id'] = $data['city_id'];
+	   	 }
+	    	if(!empty($data['name'])) {
+	    		$sdata['name'] = ['like', '%'.$data['name'].'%'];
+	    	}
 	 	//数据获取，使用关联数组的方式
 	 	$cityArrs = $categoryArrs = [];
-        $categorys = model("Category")->getNormalCategorysByParentId();
-        foreach($categorys as $category) {
+        	$categorys = model("Category")->getNormalCategorysByParentId();
+        	foreach($categorys as $category) {
         	$categoryArrs[$category->id] = $category->name;
-        }
+        	}
 
-        $citys = model("City")->getNormalCitys();
-        foreach($citys as $city) {
-        	$cityArrs[$city->id] = $city->name;
-        }
+        	$citys = model("City")->getNormalCitys();
+        	foreach($citys as $city) {
+        		$cityArrs[$city->id] = $city->name;
+        	}
 
 	 	//团购获取
 	 	$deals = $this->obj->getNormalDeals($sdata);
@@ -55,14 +55,14 @@ class Deal extends Controller
 	 		'categorys' => $categorys,
 	 			//下面填充的用于数据回显
 	 		'category_id' => empty($data['category_id']) ? '' : $data['category_id'],
-	        'city_id' => empty($data['city_id']) ? '' : $data['city_id'],
-	        'start_time' => empty($data['start_time']) ? '' : $data['start_time'],
-	        'end_time' => empty($data['end_time']) ? '' : $data['end_time'],
-	        'name' => empty($data['name']) ? '' : $data['name'],
-	        //下面的用于搜索的结果
+	        	'city_id' => empty($data['city_id']) ? '' : $data['city_id'],
+	        	'start_time' => empty($data['start_time']) ? '' : $data['start_time'],
+	        	'end_time' => empty($data['end_time']) ? '' : $data['end_time'],
+	        	'name' => empty($data['name']) ? '' : $data['name'],
+	        	//下面的用于搜索的结果
 	 		'deals' => $deals,
 	 		'categoryArrs' => $categoryArrs,
-        	'cityArrs' => $cityArrs,
+        		'cityArrs' => $cityArrs,
 	 	]);
  	}
 }
